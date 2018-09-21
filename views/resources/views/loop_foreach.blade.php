833a69af5c87d789d6ffb80cf07a3403472b79a8
Anderson Henrique da Silva <anderson.h.silva@gmail.com>
2018-09-21 16:42:56 -0300

    atributos dos lacos

--- views/resources/views/loop_foreach.blade.php
+++ views/resources/views/loop_foreach.blade.php
@@ -1,5 +1,26 @@
+@extends('layout.base')
+@section('corpo','')
+
 <h1>Loop - Foreach - Arrays Associativos</h1>
 
 @foreach ($produtos as $p)
-    <p>{{$p['id']}} - {{$p['nome']}}</p>
+    {{$p['id']}} - {{$p['nome']}}</br>
+@endforeach
+
+<hr>
+
+@foreach ($produtos as $p)
+
+  @if ($loop->first)
+    (inicia loop)</br>
+  @endif
+
+  {{$p['id']}} - {{$p['nome']}}
+  <span class="badge badge-dark">{{$loop->index}} / {{$loop->count-1}} resta -> {{$loop->remaining}}</span>
+  <span class="badge badge-warning">{{$loop->iteration}} / {{$loop->count}}</span><br>
+
+  @if ($loop->last)
+    (termina loop)</br>
+  @endif
+
 @endforeach
