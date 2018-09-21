2e1845b0be768dcb8e7c53e39992f561863599c2
Anderson Henrique da Silva <anderson.h.silva@gmail.com>
2018-09-21 18:51:34 -0300

    adicionando dados inicias ao banco com o script artisan db::ssed

---
+++ vendas/database/seeds/CategoriaSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class CategoriaSeeder extends Seeder {
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run() {
+      DB::table('categorias')->insert(['nome'=>'Roupas']);
+      DB::table('categorias')->insert(['nome'=>'Eletronicos']);
+      DB::table('categorias')->insert(['nome'=>'Perfumes']);
+      DB::table('categorias')->insert(['nome'=>'Móveis']);
+      DB::table('categorias')->insert(['nome'=>'Comidas']);
+
+    }
+
+}
