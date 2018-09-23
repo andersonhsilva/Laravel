d85acce8282f07e89b9241bdc66bc5c4855d8974
Anderson Henrique da Silva <anderson.h.silva@gmail.com>
2018-09-22 21:39:02 -0300

    CRUD completo com Eloquent / ORM

--- modelos/app/Categoria.php
+++ modelos/app/Categoria.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Categoria extends Model
 {
-    //
+    // se o nome da tabela no banco estiver no singular, tem que definir o nome dela na variavel a baixo
+    // protected $table_name = "categoria";
 }
--- modelos/routes/web.php
+++ modelos/routes/web.php
@@ -11,6 +11,47 @@
 |
 */
 
+use App\Categoria;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/insert/categoria/{nome}', function($nome) {
+  $cat = new Categoria();
+  $cat->nome = $nome;
+  $cat->save();
+  return redirect()->route('listar_categoria');
+})->name('inserir_categoria')->where('nome','[A-za-z]+');
+
+Route::get('/list/categoria/', function() {
+  $cat = Categoria::all();
+  echo json_encode($cat);
+})->name('listar_categoria');
+
+Route::get('/get/categoria/{id}', function($id) {
+  $cat = Categoria::find($id);
+  echo json_encode($cat);
+})->name('captura_categoria_id')->where('id','[0-9]+');
+
+Route::get('/get/categoria/{nome}', function($nome) {
+  $cat = Categoria::where('nome', 'like', "%$nome%")->get();
+  // retorna o resultado, só que com o json mais elaborado
+  $result[0] = ["resultado" => $cat];
+  $result[1] = ["qtd_registros" => $cat->count()];
+  $result[2] = ["id_max" => $cat->max('id')];
+  echo json_encode($result);
+})->name('captura_categoria_nome')->where('nome','[A-za-z]+');
+
+Route::get('/update/categoria/{id}/{nome}', function($id, $nome) {
+  $cat = Categoria::find($id);
+  $cat->nome = $nome;
+  $cat->save();
+  echo json_encode($cat);
+})->name('atualizar_categoria')->where('id','[0-9]+')->where('nome','[A-za-z]+');
+
+Route::get('/delete/categoria/{id}', function($id) {
+  $cat = Categoria::find($id);
+  $cat->delete();
+  return redirect()->route('listar_categoria');
+})->name('atualizar_categoria')->where('id','[0-9]+');
