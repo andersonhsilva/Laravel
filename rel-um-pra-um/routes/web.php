111007f5dc53b8dc5c93bc7c438b7dc30e012fa0
Anderson Henrique da Silva <anderson.h.silva@gmail.com>
2018-10-02 22:44:35 -0300

    subconsulta manual de relacionamento um-pra-um

---
+++ rel-um-pra-um/routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use App\Cliente;
+use App\Endereco;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/clientes', function () {
+    $clientes = Cliente::all();
+    foreach ($clientes as $cli) {
+      // consulta simples do larvel
+      echo "ID: $cli->id Nome: $cli->nome Telefone: $cli->telefone <br>";
+      // subconsulta no banco de forma manual pelo laravel
+      $end = Endereco::where('cliente_id', $cli->id)->first();
+      echo "Rua: $end->rua Numero: $end->numero Bairro: $end->bairro Cidade: $end->cidade UF: $end->uf CEP: $end->cep <br>";
+      echo "-----------------------------------------<br>";
+    }
+});
+
+Route::get('/enderecos', function () {
+    $endereco = Endereco::all();
+    foreach ($endereco as $end) {
+      echo "Rua: $end->rua Numero: $end->numero Bairro: $end->bairro Cidade: $end->cidade UF: $end->uf CEP: $end->cep <br>";
+    }
+});
